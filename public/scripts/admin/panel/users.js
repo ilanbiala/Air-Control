da2628f6324a5d94431556dd4fc2b08e637bb61d
Ilan Biala <ilan.biala@gmail.com>
2014-04-06 23:35:07 -0400

    Cleaned up user page.

--- public/scripts/admin/panel/users.js
+++ public/scripts/admin/panel/users.js
@@ -1,5 +1,21 @@
+/**
+ * TODO make changing the password and the role possible
+ */
+
 function removeUserFromTable(user) {
 	$('#' + user).remove();
+}
+
+function addUserToTable(user) {
+	var userHTMLString = '';
+	userHTMLString += '<tr id="' + user._id + '">' +
+		'<td>' + user.firstName + ' ' + user.lastName + '</td>' +
+		'<td>' + user.email + '</td>' +
+		'<td>' + user.role + '</td>' +
+		'<td>Change password</td>' +
+		'<td><a href="#" class="removeUserModalButton" data-user-id="' + user._id + '" data-reveal-id="removeUserModal"><i class="fi-x"></i></a></td>' +
+		'</tr>';
+	$('.users').append(userHTMLString);
 }
 
 $(document).ready(function() {
@@ -11,8 +27,8 @@ $(document).ready(function() {
 		whichUserToDelete = $(this).attr('data-user-id');
 	});
 
-	$('#cancelRemoveUser').on('click', function() {
-		$('#removeUserModal').foundation('reveal', 'close');
+	$('#cancelRemoveUser, #cancelAddUser').on('click', function() {
+		$(this).parent().foundation('reveal', 'close');
 	});
 
 	$('#removeUser').on('click', function() {
@@ -22,7 +38,7 @@ $(document).ready(function() {
 		}).done(function(response) {
 			if (response.success === true) {
 				$('#removeUserModal').foundation('reveal', 'close');
-				$('.alert-box.success ').trigger('open.fndtn.alert-box');
+				$('.alert-box.success').trigger('open.fndtn.alert-box').children('p.content').text('Way to go! That user is out of here!');
 				removeUserFromTable(whichUserToDelete);
 				whichUserToDelete = null;
 			}
@@ -30,8 +46,25 @@ $(document).ready(function() {
 			alert('Error: ' + err);
 			console.log(err);
 			$('#removeUserModal').foundation('reveal', 'close');
-			$('.alert-box.warning ').trigger('open.fndtn.alert-box');
+			$('.alert-box.warning').trigger('open.fndtn.alert-box').children('p.content').text('Something went wrong, please try again.');
+		});
+	});
+
+	$('#addUser').on('click', function() {
+		$.ajax({
+			url: '/panel/users/create',
+			type: 'POST'
+		}).done(function(response) {
+			if (response.success === true) {
+				$('#addUserModal').foundation('reveal', 'close');
+				$('.alert-box.success').trigger('open.fndtn.alert-box');
+				addUserToTable(response.user);
+			}
+		}).fail(function(err) {
+			alert('Error: ' + err);
+			console.log(err);
+			$('#addUserModal').foundation('reveal', 'close');
+			$('.alert-box.warning').trigger('open.fndtn.alert-box');
 		});
 	});
 });
-
--- server/config/express.js
+++ server/config/express.js
@@ -4,29 +4,29 @@
  * Module dependencies.
  */
 var express = require('express'),
-    // consolidate = require('consolidate'),
-    // mongoStore = require('connect-mongo')(express),
-    // helpers = require('view-helpers'),
-    config = require('./config')(),
-    morgan = require('morgan'), // logger
-    expressValidator = require('express-validator'),
-    mongooseConnection = require('mongoose').connection,
-    bodyParser = require('body-parser'), // access form data
-    favicon = require('static-favicon'), // favicon handler
-    sass = require('node-sass'), // sass middleware
-    appPath = process.cwd(),
-    fs = require('fs');
+	// consolidate = require('consolidate'),
+	// mongoStore = require('connect-mongo')(express),
+	// helpers = require('view-helpers'),
+	config = require('./config')(),
+	morgan = require('morgan'), // logger
+	expressValidator = require('express-validator'),
+	mongooseConnection = require('mongoose').connection,
+	bodyParser = require('body-parser'), // access form data
+	favicon = require('static-favicon'), // favicon handler
+	sass = require('node-sass'), // sass middleware
+	appPath = process.cwd(),
+	fs = require('fs');
 
 module.exports = function(app, passport, db) {
-    app.set('showStackError', true);
+	app.set('showStackError', true);
 
-    // Prettify HTML
-    app.locals.pretty = true;
+	// Prettify HTML
+	app.locals.pretty = true;
 
-    // cache=memory or swig dies in NODE_ENV=production
-    app.locals.cache = 'memory';
+	// cache=memory or swig dies in NODE_ENV=production
+	app.locals.cache = 'memory';
 
-    /*
+	/*
     // Should be placed before express.static
     // To ensure that all assets and data are compressed (utilize bandwidth)
     app.use(express.compress({
@@ -39,18 +39,18 @@ module.exports = function(app, passport, db) {
     }));
     */
 
-    // Only use logger for development environment
-    if (process.env.NODE_ENV === 'development') {
-        app.use(express.logger('dev'));
-    }
+	// Only use logger for development environment
+	if (process.env.NODE_ENV === 'development') {
+		app.use(morgan('dev'));
+	}
 
-    mongooseConnection.on('error', console.error.bind(console, 'Error:'));
+	mongooseConnection.on('error', console.error.bind(console, 'Error:'));
 
-    // The cookieParser should be above session
-    // app.use(express.cookieParser());
+	// The cookieParser should be above session
+	// app.use(express.cookieParser());
 
-    // Express/Mongo session storage
-    /*
+	// Express/Mongo session storage
+	/*
     app.use(express.session({
         secret: config.sessionSecret,
         store: new mongoStore({
@@ -60,119 +60,118 @@ module.exports = function(app, passport, db) {
     }));
     */
 
-    app.use(morgan('dev'));
-    app.use(bodyParser());
+	app.use(bodyParser());
 
-    // this line must be immediately after express.bodyParser()!
-    app.use(expressValidator([]));
+	// this line must be immediately after express.bodyParser()!
+	app.use(expressValidator([]));
 
-    // Use passport session
-    app.use(passport.initialize());
-    app.use(passport.session());
+	// Use passport session
+	app.use(passport.initialize());
+	app.use(passport.session());
 
-    app.engine('.html', require('ejs').__express);
-    
-    // set .html as the default extension
-    app.set('view engine', 'html');
-    
-    // Set views path, template engine
-    app.set('views', config.root + '/server/views');
-    
-    app.use(sass.middleware({
-        src: config.root + '/public/sass',
-        dest: config.root + '/public',
-        outputStyle: 'compressed'
-    }));
+	app.engine('.html', require('ejs').__express);
 
-    app.use(express.static(config.root + '/public'));
-    app.use(favicon(config.root + '/public/images/favicon.ico'));
+	// set .html as the default extension
+	app.set('view engine', 'html');
 
-    app.use(function(err, req, res, next) {
-        // only handle `next(err)` calls
-        if (err) {
-            console.log(err);
-            res.redirect('error');
-        }
-    });
+	// Set views path, template engine
+	app.set('views', config.root + '/server/views');
 
-    bootstrapRoutes();
+	app.use(sass.middleware({
+		src: config.root + '/public/sass',
+		dest: config.root + '/public',
+		outputStyle: 'compressed'
+	}));
 
-    // development only
-    if (config.app.enviroment == 'development') {
-        app.use(errorHandler({
-            dumpExceptions: true,
-            showStack: true
-        }));
-    }
+	app.use(express.static(config.root + '/public'));
+	app.use(favicon(config.root + '/public/images/favicon.ico'));
 
-    // TODO - move these routes into their proper place
+	app.use(function(err, req, res, next) {
+		// only handle `next(err)` calls
+		if (err) {
+			console.log(err);
+			res.redirect('error');
+		}
+	});
 
-    app.get(config.routes.home.url, function(req, res) {
-        res.render(config.routes.home.page, {
-            locals: config.routes.home.locals
-        });
-    });
+	bootstrapRoutes();
 
-    app.get(config.routes.admin.panel.url, function(req, res) {
-        res.render(config.routes.admin.panel.page, {
-            locals: config.routes.admin.panel.locals
-        });
-    });
+	// development only
+	if (config.app.enviroment == 'development') {
+		app.use(errorHandler({
+			dumpExceptions: true,
+			showStack: true
+		}));
+	}
 
-    app.get(config.routes.admin.users.url, function(req, res) {
-        res.render(config.routes.admin.users.page, {
-            locals: config.routes.admin.users.locals
-        });
-    });
+	// TODO - move these routes into their proper place
 
-    app.get(config.routes.admin.settings.url, function(req, res) {
-        res.render(config.routes.admin.settings.page, {
-            locals: config.routes.admin.settings.locals
-        });
-    });
+	app.get(config.routes.home.url, function(req, res) {
+		res.render(config.routes.home.page, {
+			locals: config.routes.home.locals
+		});
+	});
 
-    // 404 page
-    app.get(config.routes.error.notFound.url, function(req, res) {
-        res.status(404).render(config.routes.error.notFound.page, {
-            locals: config.routes.error.notFound.locals
-        });
-    });
+	app.get(config.routes.admin.panel.url, function(req, res) {
+		res.render(config.routes.admin.panel.page, {
+			locals: config.routes.admin.panel.locals
+		});
+	});
 
-    // 500 page
-    app.get(config.routes.error.server.url, function(req, res) {
-        res.status(500).render(config.routes.error.server.page, {
-            locals: config.routes.error.server.locals
-        });
-    });
+	app.get(config.routes.admin.users.url, function(req, res) {
+		res.render(config.routes.admin.users.page, {
+			locals: config.routes.admin.users.locals
+		});
+	});
 
-    app.get('/error', function(req, res) {
-        res.render('error', {
-            title: 'Node CMS | Error',
-            url: req.url,
-            originalUrl: req.originalUrl,
-            user: req.session ? req.session.user : null
-        });
-    });
+	app.get(config.routes.admin.settings.url, function(req, res) {
+		res.render(config.routes.admin.settings.page, {
+			locals: config.routes.admin.settings.locals
+		});
+	});
 
-    function bootstrapRoutes() {
-        var routes_path = appPath + '/server/routes';
-        var walk = function(path) {
-            fs.readdirSync(path).forEach(function(file) {
-                var newPath = path + '/' + file;
-                var stat = fs.statSync(newPath);
-                if (stat.isFile()) {
-                    if (/(.*)\.(js$|coffee$)/.test(file)) {
-                        console.log('Route: ', newPath);
-                        require(newPath)(app, passport);
-                    }
-                    // We skip the app/routes/middlewares directory as it is meant to be
-                    // used and shared by routes as further middlewares and is not a
-                    // route by itself
-                } else if (stat.isDirectory() && file !== 'middlewares') {
-                    walk(newPath);
-                }
-            });
-        };
-        walk(routes_path);
-    }
+	// 404 page
+	app.get(config.routes.error.notFound.url, function(req, res) {
+		res.status(404).render(config.routes.error.notFound.page, {
+			locals: config.routes.error.notFound.locals
+		});
+	});
+
+	// 500 page
+	app.get(config.routes.error.server.url, function(req, res) {
+		res.status(500).render(config.routes.error.server.page, {
+			locals: config.routes.error.server.locals
+		});
+	});
+
+	app.get('/error', function(req, res) {
+		res.render('error', {
+			title: 'Node CMS | Error',
+			url: req.url,
+			originalUrl: req.originalUrl,
+			user: req.session ? req.session.user : null
+		});
+	});
+
+	function bootstrapRoutes() {
+		var routes_path = appPath + '/server/routes';
+		var walk = function(path) {
+			fs.readdirSync(path).forEach(function(file) {
+				var newPath = path + '/' + file;
+				var stat = fs.statSync(newPath);
+				if (stat.isFile()) {
+					if (/(.*)\.(js$|coffee$)/.test(file)) {
+						console.log('Route: ', newPath);
+						require(newPath)(app, passport);
+					}
+					// We skip the app/routes/middlewares directory as it is meant to be
+					// used and shared by routes as further middlewares and is not a
+					// route by itself
+				} else if (stat.isDirectory() && file !== 'middlewares') {
+					walk(newPath);
+				}
+			});
+		};
+		walk(routes_path);
+	}
 };
